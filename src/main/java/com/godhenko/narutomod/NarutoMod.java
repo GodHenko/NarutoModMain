6aa0d2796aa1cd42be8d373288b6f479cd655657
GodTheWeab <godhenko@gmail.com>
2022-07-16 11:59:32 -0700

    back to square one

--- src/main/java/com/godhenko/narutomod/NarutoMod.java
+++ src/main/java/com/godhenko/narutomod/NarutoMod.java
@@ -1,31 +1,17 @@
 package com.godhenko.narutomod;
 
-import com.godhenko.narutomod.client.ClientProxy;
 import com.godhenko.narutomod.inits.BlockInit;
 import com.godhenko.narutomod.inits.ItemInit;
-import com.godhenko.narutomod.util.IProxy;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(NarutoMod.MOD_ID)
@@ -49,8 +35,7 @@ public class NarutoMod
             return new ItemStack(BlockInit.UCHIHA_CLOTH.get());
         }
     };
-    public static final String MODID = "narutomod";
-    public static final IProxy PROXY = DistExecutor.safeRunForDist(() -> com.github.godhenko.narutomod.util.ClientProxy::new, () -> com.github.clevernucleus.playerex.util.ServerProxy::new);
+
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
